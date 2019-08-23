1f4ae71118ff9a43514363c27ed5bff527eeba34
Gareth Clubb <gareth.clubb@telegraph.co.uk>
2019-08-23 07:28:13 +0100

    Adding crude No Deals message

--- src/scripts/ViewDeals.js
+++ src/scripts/ViewDeals.js
@@ -10,6 +10,8 @@ class ViewDeals {
     if (this.dealList && data.length) {
       const htmlToAppend = this.template.buildDealList(data);
       this.dealList.innerHTML = htmlToAppend;
+    } else {
+      this.dealList.innerHTML = 'Sorry, no deals';
     }
   }
 
